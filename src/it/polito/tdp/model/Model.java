29687c54099a43dcbb5c195ea14695604ab80198
Debora Mignogna <Debora Mignogna@tabo2018>
2019-06-20 18:08:16 +0200

    Ricorsione con due metodi diversi

--- src/it/polito/tdp/model/Model.java
+++ src/it/polito/tdp/model/Model.java
@@ -1,14 +1,92 @@
 package it.polito.tdp.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import it.polito.tdp.dao.EsameDAO;
+
 public class Model {
+	private EsameDAO dao;
+	private List<Esame> tuttiEsami;
+	private List<Esame> ottima;
+	double mediaBest=0;
+	public Model() {
+		dao=new EsameDAO();
+		tuttiEsami= new LinkedList<Esame>(dao.getTuttiEsami());
+		
+	}
+	
 
 	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		
-		System.out.println("TODO!");
+		ottima= new LinkedList<Esame>();
+		List<Esame> parziale= new LinkedList<Esame>();
+		cerca(parziale, numeroCrediti ,0);
 		
-		return null;
+		return ottima;
+	}
+	
+	private void cerca(List<Esame> parziale, int crediti, int L) {
+		
+		if(parziale.size()>=1) {
+			
+		
+		if(getCrediti(parziale)==crediti) {
+			if(getMedia(parziale)>mediaBest){
+				this.ottima= new LinkedList<Esame>(parziale);
+				mediaBest=getMedia(parziale);
+				System.out.println(mediaBest+" \n");
+			}
+		} 
+		if(getCrediti(parziale)>crediti) return;
+		
+		}
+	/*	for(Esame e: this.tuttiEsami) {
+			if(!parziale.contains(e)) {
+				if(getCrediti(parziale)+e.getCrediti()<=crediti) {
+					parziale.add(e);
+					this.cerca(parziale, crediti, 0);
+					parziale.remove(parziale.size()-1);
+					//this.cerca(parziale, crediti);
+				}
+			}
+	}*/
+		//secondo metodo
+		if(L==tuttiEsami.size()) return;
+		this.cerca(parziale, crediti, L+1);
+		parziale.add(tuttiEsami.get(L));
+		this.cerca(parziale, crediti, L+1);
+		parziale.remove(tuttiEsami.get(L));
+		
+		
+		 
+	}
+
+
+	private double getMedia(List<Esame> parziale) {
+		double media=0;
+		double somma=0;
+		if(parziale.size()==1) {
+			return parziale.get(0).getVoto();
+		}
+		int i=0;
+		for(Esame e: parziale) {
+			somma+=e.getVoto();
+			i++;
+		}
+		media=somma/i;
+		return media;
+	}
+
+
+	private int getCrediti(List<Esame> parziale) {
+		int crediti=0;
+		
+		for(Esame e: parziale) {
+			crediti+=e.getCrediti();
+			
+		}
+		return crediti;
 	}
 
 }
--- src/it/polito/tdp/model/TestModel.java
+++ src/it/polito/tdp/model/TestModel.java
@@ -3,10 +3,11 @@ package it.polito.tdp.model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		
 		Model model = new Model();
-		model.calcolaSottoinsiemeEsami(20);
+		
+		System.out.println(model.calcolaSottoinsiemeEsami(20));
 		
 	}
 
